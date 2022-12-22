83d8889b331154ae90791251dfa0044b6f249fa3
Arieh Norton <arieh.norton@gmail.com>
2022-12-22 18:40:51 +0200

    updated upload dynamically and download

--- lib/storage_service.dart
+++ lib/storage_service.dart
@@ -3,10 +3,13 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 import 'package:firebase_core/firebase_core.dart' as firebase_core;
+import 'package:google_signin/models/variables.dart';
 
 class Storage {
   final firebase_storage.FirebaseStorage storage =
       firebase_storage.FirebaseStorage.instance;
+
+  final String pathUID = currentUser.currentGroupId;
 
   Future<void> uploadFile(
     String filePath,
@@ -15,14 +18,15 @@ class Storage {
     File file = File(filePath);
 
     try {
-      await storage.ref('test/$fileName').putFile(file);
+      await storage.ref('$pathUID/$fileName').putFile(file);
     } on firebase_core.FirebaseException catch (e) {
       print(e);
     }
   }
 
   Future<firebase_storage.ListResult> listFiles() async {
-    firebase_storage.ListResult results = await storage.ref('test').listAll();
+    firebase_storage.ListResult results =
+        await storage.ref('$pathUID').listAll();
     results.items.forEach((firebase_storage.Reference ref) {
       print('Found file: $ref');
     });
@@ -30,7 +34,8 @@ class Storage {
   }
 
   Future<String> downloadURL(String imageName) async {
-    String downloadURL = await storage.ref('test/$imageName').getDownloadURL();
+    String downloadURL =
+        await storage.ref('$pathUID/$imageName').getDownloadURL();
     return downloadURL;
   }
 }
