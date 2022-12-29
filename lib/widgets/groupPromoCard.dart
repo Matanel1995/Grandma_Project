1f234a8e93b8abef2f1eb24e70d52fd46edc4c13
Arieh Norton <arieh.norton@gmail.com>
2022-12-29 20:28:56 +0200

    added icon

--- lib/widgets/groupPromoCard.dart
+++ lib/widgets/groupPromoCard.dart
@@ -11,47 +11,54 @@ class groupPromoCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      child: Container(
-        height: 80,
-        width: 1000,
-        padding: const EdgeInsets.all(4),
-        child: AspectRatio(
-          aspectRatio: 2.62 / 3,
-          child: Container(
-            margin: EdgeInsets.only(right: 15.0),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              image: DecorationImage(
-                  fit: BoxFit.cover,
-                  image: NetworkImage(currGroup.groupPhotoUrl)),
-            ),
-            child: Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  gradient: LinearGradient(
-                      begin: Alignment.bottomRight,
-                      stops: const [
-                        0.1,
-                        0.9
-                      ],
-                      colors: [
-                        Colors.black.withOpacity(.8),
-                        Colors.black.withOpacity(.1)
-                      ])),
-              child: Align(
-                alignment: const Alignment(-0.8, 0.77),
-                child: Text(
-                  currGroup.groupName,
-                  style: const TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w500,
-                    color: Colors.white,
+      child: Row(
+        children: [
+          const Icon(Icons.looks_one),
+          Expanded(
+              flex: 1,
+              child: Container(
+                height: 80,
+                width: 1000,
+                padding: const EdgeInsets.all(4),
+                child: AspectRatio(
+                  aspectRatio: 2.62 / 3,
+                  child: Container(
+                    margin: const EdgeInsets.only(right: 15.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: NetworkImage(currGroup.groupPhotoUrl)),
+                    ),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          gradient: LinearGradient(
+                              begin: Alignment.bottomRight,
+                              stops: const [
+                                0.1,
+                                0.9
+                              ],
+                              colors: [
+                                Colors.black.withOpacity(.8),
+                                Colors.black.withOpacity(.1)
+                              ])),
+                      child: Align(
+                        alignment: const Alignment(-0.8, 0.77),
+                        child: Text(
+                          currGroup.groupName,
+                          style: const TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
                   ),
                 ),
-              ),
-            ),
-          ),
-        ),
+              )),
+        ],
       ),
       onTap: () {
         Navigator.of(context).push(MaterialPageRoute(builder: (_) {
