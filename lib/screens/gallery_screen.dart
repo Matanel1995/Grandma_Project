123d56f2b5e0794977591de92715c7a0029267b6
Arieh Norton <arieh.norton@gmail.com>
2023-01-10 19:50:50 +0200

    clean up

--- lib/screens/gallery_screen.dart
+++ lib/screens/gallery_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:google_signin/screens/home_screen.dart';
 import 'package:google_signin/screens/welcome_screen.dart';
 import 'package:google_signin/storage_service.dart';
 
@@ -22,18 +23,19 @@ class _GalleryScreenState extends State<GalleryScreen> {
 
   @override
   void initState() {
-    Random rnd = Random();
-    int min = 0, max = imageTest1.length;
-
+    // Random rnd = Random();
+    // int min = 0, max = imageTest1.length;
     super.initState();
-    Timer.periodic(const Duration(seconds: 6), (timer) {
-      setState(() {
-        index = (index + 1) % imageTest1.length;
-        // int r = min + rnd.nextInt(max - min);
-        // index = r;
-        // print(r);
+    if (imageTest1.isNotEmpty) {
+      Timer.periodic(const Duration(seconds: 6), (timer) {
+        setState(() {
+          index = (index + 1) % imageTest1.length;
+          // int r = min + rnd.nextInt(max - min);
+          // index = r;
+          // print(r);
+        });
       });
-    });
+    }
   }
 
   @override
